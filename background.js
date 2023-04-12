842f95205a72e112f8ecf32d3552bfc98a28fe59
S1monlol <simonn@duck.com>
2023-04-11 20:26:48 -0400

    haha oops mb (added back auto deleting chats)

--- background.js
+++ background.js
@@ -240,28 +240,28 @@ async function deleteConvo(token) {
 
     console.log(id)
 
-    // let deleteResponse = await fetch(`https://chat.openai.com/backend-api/conversation/${id}`, {
-    //     "headers": {
-    //         "accept": "text/event-stream",
-    //         "accept-language": "en-US,en;q=0.9",
-    //         "authorization": `Bearer ${token}`,
-    //         "content-type": "application/json",
-    //         "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
-    //         "sec-ch-ua-mobile": "?0",
-    //         "sec-ch-ua-platform": "\"macOS\"",
-    //         "sec-fetch-dest": "empty",
-    //         "sec-fetch-mode": "cors",
-    //         "sec-fetch-site": "same-origin",
-    //         "x-openai-assistant-app-id": "",
-    //         "origin": "https://chat.openai.com"
-    //     },
-    //     "method": "PATCH",
-    //     "body": `{"is_visible":false}`
-    // })
+    let deleteResponse = await fetch(`https://chat.openai.com/backend-api/conversation/${id}`, {
+        "headers": {
+            "accept": "text/event-stream",
+            "accept-language": "en-US,en;q=0.9",
+            "authorization": `Bearer ${token}`,
+            "content-type": "application/json",
+            "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-openai-assistant-app-id": "",
+            "origin": "https://chat.openai.com"
+        },
+        "method": "PATCH",
+        "body": `{"is_visible":false}`
+    })
 
-    // let deleteBody = await deleteResponse.json()
+    let deleteBody = await deleteResponse.json()
 
-    // console.log(deleteResponse, deleteBody)
+    console.log(deleteResponse, deleteBody)
 }
 
 // Listen for messages from content script
