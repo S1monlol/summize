fca0ccc56fec141c4aacc2accbf3d4fae81ca2ae
S1monlol <simonn@duck.com>
2023-04-11 20:18:24 -0400

    made it not terrible

--- background.js
+++ background.js
@@ -33,6 +33,7 @@ async function getToken() {
 }
 
 let targetTabId; // The ID of the tab where the content script is running
+let oldMessageId
 
 
 async function handleStreamedResponse(token, question, videoId, parts) {
@@ -44,6 +45,7 @@ async function handleStreamedResponse(token, question, videoId, parts) {
     console.log("question: " + question)
     let subs
     let questionParts
+    let multipart = false
 
     // if the question is more than 15k characters, then we need to split it into multiple requests
 
@@ -53,15 +55,15 @@ async function handleStreamedResponse(token, question, videoId, parts) {
     for (let i = parts.length; i < questionParts.length; i++) {
         if (i == 0) {
             console.log("first part")
-
             if (question.length > 15000) {
-                subs = `This is the first part of the transcript. Keep it to a super short summary, no more than 3 sentences (dont mention that you're reading a transcript): ` + questionParts[i]
+                subs = `This is the first part of the transcript. Keep it to a super short summary, no more than 3 sentences. Dont mention that you're reading a transcript, and exlude any parts about a sponsership: ` + questionParts[i]
             } else {
                 subs = "this is a youtube video's transcript. Short summary (dont mention that you're reading a transcript): " + questionParts[i]
             }
             break
         } else {
             subs = `Previous summary: ${parts}. This is the ${i + 1} part of the transcript. Rewritten, continued summary (dont mention that you're reading a transcript): ` + questionParts[i]
+            multipart = true
             break
         }
     }
@@ -69,9 +71,36 @@ async function handleStreamedResponse(token, question, videoId, parts) {
 
     console.log("subs ", subs)
 
-    // let subs = "this is a youtube video's transcript. Short summary (dont mention that you're reading a transcript): " + question
-    let body = { "action": "next", "messages": [{ "id": "73954541-ff9e-4785-85e4-d45527ccca73", "author": { "role": "user" }, "content": { "content_type": "text", "parts": [subs] } }], "parent_message_id": uuid1, "model": "text-davinci-002-render-sha", "timezone_offset_min": 240 }
-    // let body = { "action": "next", "messages": [{ "id": "171c9ee7-ce8d-4056-acf4-75974ee8c8fe", "role": "user", "content": { "content_type": "text", "parts": [subs] } }], "parent_message_id": "425da9a9-246e-433e-8d9a-b435da2e5e14", "model": "text-davinci-002-render" }
+    let body
+    if (!multipart) {
+        body = { "action": "next", "messages": [{ "id": "73954541-ff9e-4785-85e4-d45527ccca73", "author": { "role": "user" }, "content": { "content_type": "text", "parts": [subs] } }], "parent_message_id": uuid1, "model": "text-davinci-002-render-sha", "timezone_offset_min": 240 }
+    } else {
+        let response = await fetch("https://chat.openai.com/backend-api/conversations?offset=0&limit=20", {
+            "headers": {
+                "accept": "text/event-stream",
+                "accept-language": "en-US,en;q=0.9",
+                "authorization": `Bearer ${token}`,
+                "content-type": "application/json",
+                "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"macOS\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-openai-assistant-app-id": "",
+                "origin": "https://chat.openai.com"
+            },
+        })
+
+        let body2 = await response.json()
+
+        console.log(response, body2)
+
+        let id = body2.items[0].id
+
+        console.log(id)
+        body = { "action": "next", "conversation_id": id, "messages": [{ "id": "73954541-ff9e-4785-85e4-d45527ccca73", "author": { "role": "user" }, "content": { "content_type": "text", "parts": [subs] } }], "parent_message_id": uuid1, "model": "text-davinci-002-render-sha", "timezone_offset_min": 240 }
+    }
     body = JSON.stringify(body);
     const response = await fetch("https://chat.openai.com/backend-api/conversation", {
         "headers": {
@@ -125,7 +154,7 @@ async function handleStreamedResponse(token, question, videoId, parts) {
 
             if (chunks[0].includes("[DONE]")) {
 
-                if(parts.length != questionParts.length){
+                if (parts.length != questionParts.length) {
                     // add the last final text to the parts array
                     parts.push(chunks[1])
                     console.log("parts: ", parts)
